37cc2ab3adefe650b150e1270faad1d5b56d7ef5
Patriciapilre <patriciapilre@amatoscar.pt>
2025-01-30 22:12:27 +0000

    ex

---
+++ Exercicio4.py
@@ -0,0 +1,13 @@
+#olá, vamos começar
+
+#exercicio 4:
+
+# Define os valores de a e b
+a = 3
+b = 5
+
+# Calcula a expressão 2a x 3b
+resultado = 2 * a * 3 * b
+
+# Escreve a expressão e o resultado
+print("2a x 3b é igual a {resultado}")
