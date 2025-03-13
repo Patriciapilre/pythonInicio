81294082170272934471184491a0ef1681c6a2be
Patriciapilre <patriciapilre@amatoscar.pt>
2025-03-13 20:02:01 +0000

    test

--- Exercicios/Exercicio4.py
+++ Exercicios/Exercicio4.py
@@ -11,3 +11,19 @@ resultado = 2 * a * 3 * b
 
 # Escreve a expressão e o resultado
 print("2a x 3b é igual a {resultado}")
+
+
+
+# pedir ao utilizador que insira a data de nascimento
+data_nascimento = input("Por favor, insira a sua data de nascimento (DD/MM/AAAA): ")
+
+# Aqui divide a string da data em dia, mês e ano com a funcao split
+dia, mes, ano = data_nascimento.split('/')
+
+# Converte o dia, mes e ano para um número inteiro
+dia = int(dia)
+mes = int(mes)
+ano_inteiro = int(ano)
+
+# Exibe a data como um número inteiro
+print(f"a data de nascimento é: {dia} / {mes} / {ano_inteiro}")
